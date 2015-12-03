d157556395ba09c9742162b5b86724d26a451c30
Viet Pham <viet.pham@audiencemedia.com>
2015-12-03 11:46:08 +0700

    input should not be changed

--- godsv_test.go
+++ godsv_test.go
@@ -16,26 +16,34 @@ func ExampleMarshal() {
 		"ef\\gh",
 	}
 	fmt.Println(Marshal(sample))
+	fmt.Println(strings.Join(sample, " "))
 	// Output: abc:def:ghk::ab\:cd:ef\\gh
+	// abc def ghk  ab:cd ef\gh
 }
 
 func ExampleUnmarshal() {
 	sample := "abc:def:ghk::ab\\:cd:ef\\\\gh"
 	result := Unmarshal(sample)
 	fmt.Println(strings.Join(result, " "))
+	fmt.Println(sample)
 	// Output: abc def ghk  ab:cd ef\gh
+	// abc:def:ghk::ab\:cd:ef\\gh
 }
 
 func ExampleCount() {
 	sample := "abc:def:ghk::ab\\:cd:ef\\\\gh"
 	fmt.Printf("%d\n", count(sample))
+	fmt.Println(sample)
 	// Output: 6
+	// abc:def:ghk::ab\:cd:ef\\gh
 }
 
 func ExampleClean() {
 	sample := "ab\\:cd\\\\e"
 	fmt.Println(clean(sample))
+	fmt.Println(sample)
 	// Output: ab:cd\e
+	// ab\:cd\\e
 }
 
 func BenchmarkMarshal(b *testing.B) {
